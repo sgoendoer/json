9089b7548a22bb6ae0c3020c548e8a7645fe007b
Sebastian Göndör <sebastian.goendoer@tu-berlin.de>
2015-12-02 17:37:34 +0100

    initial commit

---
+++ src/JSONArray.php
@@ -0,0 +1,360 @@
+<?php namespace sgoendoer\json;
+
+use sgoendoer\json\JSONObject;
+use sgoendoer\json\JSONException;
+
+class JSONArray
+{
+	/**
+	 * The array where the JSONArray's properties are kept.
+	 */
+	private $map;
+
+	/**
+	 * Construct a JSONArray.
+	 * 
+	 * @param $param An optional parameter to create the JSONArray from
+	 */
+	public function __construct($param = NULL)
+	{
+		$this->map = array();
+		
+		if($param == NULL)
+		{
+			
+		}
+		// if param == JSON Object => create JSONArray from JSONObjects parameters
+		elseif($param instanceof JSONObject)
+		{
+			foreach($param->keys() as $key)
+			{
+				$this->put($param->get($key));
+			}
+		}
+		// if param == JSON Object => create copy
+		elseif($param instanceof JSONArray)
+		{
+			foreach($param->keys() as $key)
+			{
+				$this->put($param->get($key));
+			}
+		}
+		// if param == object => create object with keys
+		elseif(gettype($param) == 'object')
+		{
+			if($keys == NULL || !is_array($keys))
+				$keys = array_keys(get_object_vars($param));
+			
+			// case non-empty object
+			if(count($keys) != 0)
+			{
+				foreach(get_object_vars($param) as $key => $value)
+				{
+					if(in_array((string) $key, $keys))
+					{
+						
+						if(gettype($value) == 'object')
+						{
+							$this->put(new JSONObject($value), (string) $key);
+						}
+						elseif(gettype($value) == 'array')
+						{
+							$this->put(new JSONArray($value), (string) $key);
+						}
+						else
+						{
+							$this->put($value, (string) $key);
+						}
+					}
+				}
+			}
+		}
+		elseif(is_array($param))
+		{
+			foreach($param as $value)
+			{
+				if(gettype($value) == 'object')
+				{
+					$this->put(new JSONObject($value));
+				}
+				elseif(gettype($value) == 'array')
+				{
+					$this->put(new JSONArray($value));
+				}
+				else
+				{
+					$this->put($value);
+				}
+			}
+		}
+		elseif(gettype($param) == 'string')
+		{
+			$tmp = json_decode($param);
+			
+			if(json_last_error() != JSON_ERROR_NONE)
+				throw new JSONException(json_last_error_msg());
+			
+			if(!is_array($tmp))
+				throw new JSONException('A JSONArray text must start with "["');
+			
+			foreach($tmp as $value)
+			{
+				$this->put($value);
+			}
+		}
+		else
+		{
+			throw new JSONException("JSONArray initial value should be a string or array.");
+		}
+	}
+	
+	/**
+	 * Get the value associated with a key.
+	 *
+	 * @param key A key string.
+	 * @return The object associated with the key.
+	 * @throws JSONException if the key is not found.
+	 */
+	public function get($key)
+	{
+		if($key == NULL || $key == '')
+		{
+			throw new JSONException('Null key.');
+		}
+		
+		$object = $this->opt($key);
+		
+		if($object == NULL)
+		{
+			throw new JSONException('JSONArray[' . self::quote($key) . '] not found.');
+		}
+		
+		return $object;
+	}
+	
+	// public function getInt(){}
+	// public function getFloat(){}
+	// public function getString(){}
+	// public function getBoolean(){}
+	// public function getJSONArray(){}
+	// public function getJSONObject(){}
+	
+	/**
+	 * Put a key/value pair in the JSONArray.
+	 *
+	 * @param value The value to put
+	 * @param key An optional key
+	 * @return this
+	 * @throws JSONException If the value is not a valid value
+	 */
+	public function put($value, $key = NULL)
+	{
+		// disallow non-integer keys
+		if($key != NULL && gettype($key) != 'integer')
+		{
+			throw new JSONException('Key value must be an integer value');
+		}
+		
+		if(gettype($value) == 'object' && !($value instanceof JSONObject) && !($value instanceof JSONArray))
+		{
+			$value = new JSONObject($value);
+		}
+		elseif(gettype($value) == 'array')
+		{
+			$value = new JSONArray($value);
+		}
+		
+		if($value !== NULL) // "", 0, 0.0, false, etc evaluate to NULL
+		{
+			$this->testValidity($value);
+			
+			if($key != NULL && in_array($key, $this->map))
+				$this->map[$key] = $value;
+			else
+				$this->map[] = $value;
+		}
+		else
+		{
+			throw new JSONException('Null value');
+		}
+		
+		return $this;
+	}
+	
+	// public function putInt(){}
+	// public function putFloat(){}
+	// public function putString(){}
+	// public function putBoolean(){}
+	// public function putJSONArray(){}
+	// public function putJSONObject(){}
+	
+	/**
+	 * Get an optional value associated with a key.
+	 *
+	 * @param key A key string.
+	 * @return An object which is the value, or NULL if there is no value.
+	 */
+	public function opt($key)
+	{
+		if($key == NULL || $key == '' || !array_key_exists($key, $this->map))
+		{
+			return NULL;
+		}
+		else
+		{
+			return $this->map[$key];
+		}
+	}
+	
+	// public function optInt(){}
+	// public function optFloat(){}
+	// public function optString(){}
+	// public function optBoolean(){}
+	// public function optJSONArray(){}
+	// public function optJSONObject(){}
+	
+	/**
+	 * Removes an index
+	 *
+	 * @param index The index of the element to be removed.
+	 * @return The value that was associated with the index, or null if there was no value.
+	 */
+	public function remove($index)
+	{
+		$value = $this->opt($index);
+		unset($this->map[$index]);
+		
+		return $value;
+	}
+	
+	/**
+	 * Get an array of the keys of the JSONObject.
+	 *
+	 * @return An array with the keys
+	 */
+	public function keys()
+	{
+		return array_keys($this->map);
+	}
+	
+	/**
+	 * Produces a JSONObject from the JSONArray
+	 *
+	 * @param names An array containing a list of key strings. These will be paired with the values. If $names contains
+	 * less values than the source JSONArray, remaining values will be ommitted.
+	 * @return A JSONObject, or null if there are no names or if this JSONArray has no values.
+	 * @throws JSONException If any of the names are null.
+	 */
+	public function toJSONObject($names)
+	{
+		if(!is_array($names))
+			throw new JSONException('Provided value must be an array');
+		
+		if($names == NULL || count($names) == 0 || $this->length() == 0)
+		{
+			return NULL;
+		}
+		
+		$jsonObject = new JSONObject();
+		
+		foreach($this->map as $index => $value)
+		{
+			$jsonObject->put($value, $names[$index]);
+		}
+		
+		return $jsonObject;
+	}
+	
+	/**
+	 * Throw an exception if the object is not a valid JSON value
+	 *
+	 * @param object The object to test.
+	 * @throws JSONException If the object is not a valid JSON value
+	 */
+	public static function testValidity($object)
+	{
+		switch(gettype($object))
+		{
+			case 'integer':
+			case 'double': // is returned when $object is a float
+				if(is_nan($object) || is_infinite($object))
+					throw new JSONException('JSON does not allow non-finite numbers.');
+			break;
+			
+			case 'boolean':
+			break;
+			
+			case 'string':
+				// TODO check for unescaped chars
+			break;
+			
+			case 'NULL':
+			break;
+			
+			case 'array':
+				throw new JSONException('JSON does not allow values of type ' . gettype($object));
+			break;
+			
+			case 'object':
+				if(!($object instanceof JSONObject) && !($object instanceof JSONArray))
+					throw new JSONException('JSON does not allow values of type ' . gettype($object));
+			break;
+			
+			case 'resource':
+			case 'unknown type':
+			default:
+				throw new JSONException('JSON does not allow values of type ' . gettype($object));
+			break;
+		}
+	}
+	
+	/**
+	 * Write the contents of the JSONArray as JSON text to a writer. For
+	 * compactness, no whitespace is added.
+	 * <p>
+	 * Warning: This method assumes that the data structure is acyclical.
+	 *
+	 * @return The writer.
+	 * @throws JSONException
+	 */
+	public function write()
+	{
+		$returnstring = '[';
+		
+		foreach($this->map as $value)
+		{
+			if($value instanceof JSONObject || $value instanceof JSONArray)
+				$returnstring .= $value->write();
+			elseif(gettype($value) == 'integer' || gettype($value) == 'double')
+				$returnstring .= $value;
+			elseif(gettype($value) == 'boolean')
+				$returnstring .= (($value) ? 'true' : 'false');
+			elseif(gettype($value) == 'NULL')
+				$returnstring .= 'null';
+			elseif(gettype($value) == 'string')
+				$returnstring .= '"' . $value . '"';
+			else
+				throw new JSONException('Invalid type ' . gettype($value));
+			
+			if($value !== end($this->map)) $returnstring .= ',';
+		}
+		
+		$returnstring .= ']';
+		
+		return $returnstring;
+	}
+	
+	public function __toString()
+	{
+		try
+		{
+			return $this->write();
+		}
+		catch (\Exception $e)
+		{
+			//die($e->getMessage());
+		}
+	}
+}
+
+?>
---
+++ src/JSONObject.php
@@ -0,0 +1,549 @@
+<?php namespace sgoendoer\json;
+
+use sgoendoer\json\JSONArray;
+use sgoendoer\json\JSONException;
+
+class JSONObject
+{
+	/**
+	 * The array where the JSONObject's properties are kept.
+	 */
+	private $map;
+	
+	/**
+	 * Construct a JSONObject from a parameter.
+	 * 
+	 * @param $param The parameter to create a JSONObject from.
+	 * @param $keys An optional array of keys to use for JSONObject creation
+	 * @throws JSONException if the provided parameter is not a valid value.
+	 */
+	public function __construct($param = NULL, $keys = NULL)
+	{
+		$this->map = array();
+		
+		// if param == NULL => new empty object
+		if($param == NULL)
+		{}
+		// if param == JSON Object => create copy
+		elseif($param instanceof JSONObject)
+		{
+			foreach($param->keys() as $key)
+			{
+				$this->put($param->get((string) $key), (string) $key);
+			}
+		}
+		// if param == JSONArray => create JSONObject
+		elseif(($param instanceof JSONArray))
+		{
+			if($keys == NULL)
+				$keys = $param->keys();
+			
+			foreach($keys as $key)
+			{
+				$this->put($param->opt($key), (string) $key);
+			}
+		}
+		// if param == assoc array => create JSONObject
+		elseif(is_array($param))
+		{
+			foreach($param as $key => $value)
+			{
+				if(gettype($value) == 'string')
+				{
+					$this->put($value, (string) $key);
+				}
+				elseif(gettype($value) == 'object')
+				{
+					$this->put(new JSONObject($value), (string) $key);
+				}
+				elseif(gettype($value) == 'array')
+				{
+					$this->put(new JSONArray($value), (string) $key);
+				}
+				elseif(gettype($value) == 'bool')
+				{
+					$this->put($value, (string) $key);
+				}
+				elseif(gettype($value) == 'int' || gettype($value) == 'double')
+				{
+					$this->put($value, (string) $key);
+				}
+				else
+				{
+					throw new JSONException('Value [' . self::quote($value) . '] is not a valid value');
+				}
+			}
+		}
+		// if param == object => create object with keys
+		elseif(gettype($param) == 'object')
+		{
+			if($keys == NULL || !is_array($keys))
+				$keys = array_keys(get_object_vars($param));
+			
+			// case non-empty object
+			if(count($keys) != 0)
+			{
+				foreach(get_object_vars($param) as $key => $value)
+				{
+					if(in_array((string) $key, $keys))
+					{
+						
+						if(gettype($value) == 'object')
+						{
+							$this->put(new JSONObject($value), (string) $key);
+						}
+						elseif(gettype($value) == 'array')
+						{
+							$this->put(new JSONArray($value), (string) $key);
+						}
+						else
+						{
+							$this->put($value, (string) $key);
+						}
+					}
+				}
+			}
+		}
+		// if param == string => deserialize and work with result
+		elseif(gettype($param) == 'string')
+		{
+			$tmp = json_decode($param);
+			
+			if(json_last_error() != JSON_ERROR_NONE)
+				throw new JSONException(json_last_error_msg());
+			
+			if(gettype($tmp) != 'object')
+				throw new JSONException('Provided string does not contain a valid JSON Object');
+			
+			foreach($tmp as $key => $value)
+			{
+				if(gettype($value) == 'string')
+				{
+					$this->put($value, (string) $key);
+				}
+				elseif(gettype($value) == 'object')
+				{
+					$this->put(new JSONObject($value), (string) $key);
+				}
+				elseif(gettype($value) == 'array')
+				{
+					$this->put(new JSONArray($value), (string) $key);
+				}
+				elseif(gettype($value) == 'bool')
+				{
+					$this->put($value, (string) $key);
+				}
+				elseif(gettype($value) == 'int' || gettype($value) == 'double')
+				{
+					$this->put($value, (string) $key);
+				}
+				else
+				{
+					throw new JSONException('Value [' . self::quote($value) . ']is not a valid value');
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Get the value associated with a key.
+	 *
+	 * @param key A key string.
+	 * @return The object associated with the key.
+	 * @throws JSONException if the key is not found.
+	 */
+	public function get($key)
+	{
+		if($key == NULL || $key == '')
+		{
+			throw new JSONException('Null key.');
+		}
+		
+		$object = $this->opt($key);
+		
+		if($object == NULL)
+		{
+			throw new JSONException('JSONObject[' . self::quote($key) . '] not found.');
+		}
+		
+		return $object;
+	}
+	
+	// public function getInt(){}
+	// public function getFloat(){}
+	// public function getString(){}
+	// public function getBoolean(){}
+	// public function getJSONArray(){}
+	// public function getJSONObject(){}
+	
+	/**
+	 * Put a key/value pair in the JSONObject.
+	 *
+	 * @param value A boolean which is the value.
+	 * @param key A key string.
+	 * @return this.
+	 * @throws JSONException If the key is null or the value is not a valid value
+	 */
+	public function put($value, $key)
+	{
+		if(gettype($value) == 'object' && !($value instanceof JSONObject) && !($value instanceof JSONArray))
+		{
+			$value = new JSONObject($value);
+		}
+		if($key == '' || $key == NULL)
+			throw new JSONException("Null key.");
+		
+		if($value !== NULL) // "", 0, 0.0, false, etc evaluate to NULL
+		{
+			$this->testValidity($value);
+			$this->map[$key] = $value;
+		}
+		else
+		{
+			$this->remove($key);
+		}
+		
+		return $this;
+	}
+	
+	// public function putInt(){}
+	// public function putFloat(){}
+	// public function putString(){}
+	// public function putBoolean(){}
+	// public function putJSONArray(){}
+	// public function putJSONObject(){}
+	
+	/**
+	 * Get an optional value associated with a key.
+	 *
+	 * @param key A key string.
+	 * @return An object which is the value, or null if there is no value.
+	 */
+	public function opt($key)
+	{
+		if($key == NULL || $key == '' || !array_key_exists($key, $this->map))
+		{
+			return NULL;
+		}
+		else
+		{
+			return $this->map[$key];
+		}
+	}
+	
+	// public function optInt(){}
+	// public function optFloat(){}
+	// public function optString(){}
+	// public function optBoolean(){}
+	// public function optJSONArray(){}
+	// public function optJSONObject(){}
+	
+	/**
+	 * Determine if the JSONObject contains a specific key.
+	 *
+	 * @param key: A key string.
+	 * @return true if the key exists in the JSONObject.
+	 */
+	public function has($key)
+	{
+		return array_key_exists($key, $this->map);
+	}
+	
+	/**
+	 * Remove a name and its value, if present.
+	 *
+	 * @param key The name to be removed.
+	 * @return The value that was associated with the name, or null if there was no value.
+	 */
+	public function remove($key)
+	{
+		if(array_key_exists($key, $this->map))
+		{
+			$tmp = $this->map[$key]; 
+			unset($this->map[$key]);
+			return $tmp;
+		}
+		else
+			return NULL;
+	}
+	
+	/**
+	 * Get an array of the keys of the JSONObject.
+	 *
+	 * @return An array with the keys
+	 */
+	public function keys()
+	{
+		return array_keys($this->map);
+	}
+	
+	/**
+	 * Produce a string in double quotes with backslash sequences in all the right places. A backslash will be inserted 
+	 * within </, producing <\/, allowing JSON text to be delivered in HTML. In JSON text, a string cannot contain a 
+	 * control character or an unescaped quote or backslash.
+	 *
+	 * @param string A string
+	 * @return A string correctly formatted for insertion in a JSONObject or JSONArray.
+	 */
+	public static function quote($string)
+	{
+		// TODO escape all the chars
+		return $string;
+	}
+	
+	/**
+	 * Try to convert a string into a number, boolean, or null. If the string can't be converted, return the string.
+	 *
+	 * @param string A String.
+	 * @return A simple JSON value.
+	 */
+	public static function stringToValue($string)
+	{
+		if($string == '')
+		{
+			return $string;
+		}
+		if(is_float($string))
+		{
+			return (float) $string;
+		}
+		if(is_int($string))
+		{
+			return (int) $string;
+		}
+		if(strtolower($string) == 'true')
+		{
+			return true;
+		}
+		if(strtolower($string) == 'false')
+		{
+			return false;
+		}
+		if(strtolower($string) == 'null')
+		{
+			return NULL;
+		}
+		
+		return $string;
+	}
+	
+	/**
+	 * Make a JSON text of an Object value. If the value is an array, then a JSONArray will be made from it and its
+	 * write() method will be called. If the value is an associative array or an object, then a JSONObject will be made 
+	 * from it and its write() method will be called. If the value is an object, the value's toString method will be 
+	 * called, and the result will be quoted.
+	 * <p>
+	 * Warning: This method assumes that the data structure is acyclical.
+	 *
+	 * @param value The value to be serialized.
+	 * @return a printable, displayable, transmittable representation of the object, beginning with <code>{</code>&nbsp;
+	 * <small>(left brace)</small> and ending with <code>}</code>&nbsp;<small>(right brace)</small>.
+	 * @throws JSONException If the value cannot be coerced to a string value.
+	 */
+	public static function valueToString($value)
+	{
+		if($value == NULL)
+		{
+			return 'null';
+		}
+		
+		if(is_numeric($value))
+		{
+			return self::numberToString($value);
+		}
+		
+		if(is_string($value))
+		{
+			return $value;
+		}
+		
+		if($value instanceof JSONObject	|| $value instanceof JSONArray)
+		{
+			return $value->write();
+		}
+		
+		if(is_array($value))
+		{
+			if(self::isAssoc($value))
+				return (new JSONObject($value))->write();
+			else
+				return (new JSONArray($value))->write();
+		}
+		
+		if(is_object($value))
+			return (new JSONObject($value))->write();
+		
+		throw new JSONException('Provided value cannot be coerced to a string');
+	}
+	
+	/**
+	 * Determines if an value is an associative array
+	 * 
+	 * @param $value The value to check
+	 * @return true, if the value is an associative array, else false
+	 */
+	private static function isAssoc($value)
+	{
+		if(!is_array($value)) return false;
+		return array_keys($value) !== range(0, count($value) - 1);
+	}
+	
+	/**
+	 * Produce a string from a number.
+	 *
+	 * @param number A number
+	 * @return A string.
+	 * @throws JSONException If $number is a non-finite number.
+	 */
+	public static function numberToString($number = NULL)
+	{
+		if($number == NULL)
+		{
+			throw new JSONException('Null pointer');
+		}
+		
+		self::testValidity($number);
+		
+		// Shave off trailing zeros and decimal point, if possible.
+		$number = (string) $number;
+		if(strpos($number, '.') > 0 && strpos($number, 'e') < 0 && strpos($number, 'E') < 0)
+		{
+			while(substr($number, -1) == '0')
+			{
+				$number = substr(0, strlen($number) - 1);
+			}
+			
+			if(substr($number, -1) == '.')
+			{
+				$number = substr(0, strlen($number) - 1);
+			}
+		}
+		
+		return $number;
+	}
+	
+	/**
+	 * Throw an exception if the object is not a valid JSON value
+	 *
+	 * @param object The object to test.
+	 * @throws JSONException If the object is not a valid JSON value
+	 */
+	public static function testValidity($object)
+	{
+		switch(gettype($object))
+		{
+			case 'integer':
+			case 'double': // is returned when $object is a float
+				if(is_nan($object) || is_infinite($object))
+					throw new JSONException('JSON does not allow non-finite numbers.');
+			break;
+			
+			case 'boolean':
+			break;
+			
+			case 'string':
+				// TODO check for unescaped chars
+			break;
+			
+			case 'NULL':
+			break;
+			
+			case 'array':
+				throw new JSONException('JSON does not allow values of type ' . gettype($object));
+			break;
+			
+			case 'object':
+				if(!($object instanceof JSONObject) && !($object instanceof JSONArray))
+					throw new JSONException('JSON does not allow values of type ' . gettype($object));
+			break;
+			
+			case 'resource':
+			case 'unknown type':
+			default:
+				throw new JSONException('JSON does not allow values of type ' . gettype($object));
+			break;
+		}
+	}
+	
+	/**
+	 * Produce a JSONArray containing the values of the members of this JSONObject.
+	 *
+	 * @param names A JSONArray containing a list of key strings. This determines the sequence of the values in the result.
+	 * @return A JSONArray of values.
+	 * @throws JSONException If any of the values are non-finite numbers.
+	 */
+	public function toJSONArray($names = NULL)
+	{
+		if($names == NULL || count($names) == 0)
+		{
+			return NULL;
+		}
+		
+		$ja = new JSONArray();
+		
+		foreach($this->map as $key => $value)
+		{
+			$ja->put($this->opt($key));
+		}
+		
+		return $ja;
+	}
+	
+	/**
+	 * Returns the contents of the JSONObject as a JSONString. For compactness, no whitespace is added.
+	 * <p>
+	 * Warning: This method assumes that the data structure is acyclical.
+	 *
+	 * @return string
+	 */
+	public function write()
+	{
+		$returnstring = '{';
+		
+		foreach($this->map as $key => $value)
+		{
+			$returnstring .= '"' . $key . '":';
+			
+			if($value instanceof JSONObject || $value instanceof JSONArray)
+				$returnstring .= $value->write();
+			elseif(gettype($value) == 'integer' || gettype($value) == 'double')
+				$returnstring .= $value;
+			elseif(gettype($value) == 'boolean')
+				$returnstring .= (($value) ? 'true' : 'false');
+			elseif(gettype($value) == 'NULL')
+				$returnstring .= 'null';
+			elseif(gettype($value) == 'string')
+				$returnstring .= '"' . $value . '"';
+			else
+				throw new JSONException('Invalid type ' . gettype($value));
+			
+			if($value !== end($this->map)) $returnstring .= ',';
+		}
+		
+		$returnstring .= '}';
+		
+		return $returnstring;
+	}
+	
+	/**
+	 * Returns the contents of the JSONObject as a JSONString. For compactness, no whitespace is added.
+	 * In case an Exception is thrown, the execution is halted.
+	 * <p>
+	 * Warning: This method assumes that the data structure is acyclical.
+	 *
+	 * @return string
+	 */
+	public function __toString()
+	{
+		try
+		{
+			return $this->write();
+		}
+		catch (\Exception $e)
+		{
+			//die($e->getMessage());
+		}
+	}
+}
+
+?>
