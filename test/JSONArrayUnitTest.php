20b7033519c095a43ef374da25853e61d59e5a4a
Sebastian Göndör <sebastian.goendoer@tu-berlin.de>
2016-01-11 14:17:31 +0100

    added PHPUnit tests for JSONArray

--- test/JSONArrayUnitTest.php
+++ test/JSONArrayUnitTest.php
@@ -10,9 +10,35 @@ class JSONArrayUnitTest extends PHPUnit_Framework_TestCase
 	public function test()
 	{
 		$jsonString = '[]';
+		$jsonStringString = '["a"]';
+		$jsonStringInteger = '[2]';
+		$jsonStringBool = '[true]';
+		$jsonStringNull = '[null]';
+		$jsonStringObject = '[{}]';
+		$jsonStringArray = '[[]]';
 		
+		// serialization // creation from string
 		$this->assertEquals($jsonString, (new JSONArray())->__toString());
+		
 		$this->assertEquals(json_decode($jsonString), (new JSONArray())->toArray());
+		$this->assertEquals(json_decode($jsonStringString), (new JSONArray($jsonStringString))->toArray());
+		$this->assertEquals(json_decode($jsonStringInteger), (new JSONArray($jsonStringInteger))->toArray());
+		$this->assertEquals(json_decode($jsonStringBool), (new JSONArray($jsonStringBool))->toArray());
+		$this->assertEquals(json_decode($jsonStringNull), (new JSONArray($jsonStringNull))->toArray());
+		$this->assertEquals(json_decode($jsonStringObject), (new JSONArray($jsonStringObject))->toArray());
+		$this->assertEquals(json_decode($jsonStringArray), (new JSONArray($jsonStringArray))->toArray());
+		
+		// building arrays
+		$a = new JSONArray();
+		$a->put('x');
+		$a->put(new JSONArray());
+		
+		$a1 = '["x",[]]';
+		$this->assertEquals($a1, $a->write());
+		
+		// accessing parameters
+		$this->assertEquals("x", $a->get(0));
+		$this->assertEquals("[]", $a->get(1));
 	}
 }
 
--- test/JSONObjectUnitTest.php
+++ test/JSONObjectUnitTest.php
@@ -11,17 +11,44 @@ class JSONObjectUnitTest extends PHPUnit_Framework_TestCase
 	{
 		$jsonString = '{}';
 		$jsonStringString = '{"a": "a"}';
-		$jsonStringInteger = '{"1": 1}';
+		$jsonStringInteger = '{"b": 1}';
+		$jsonStringBool = '{"c": true}';
+		$jsonStringNull = '{"d": null}';
+		$jsonStringObject = '{"e": {}}';
+		$jsonStringArray = '{"f": []}';
 		
+		// serialization // creation from string
 		$this->assertEquals($jsonString, (new JSONObject())->__toString());
 		$this->assertEquals(json_decode($jsonString), (new JSONObject())->toStdClass());
-		
 		$this->assertEquals(json_decode($jsonStringString), (new JSONObject($jsonStringString))->toStdClass());
 		$this->assertEquals(json_decode($jsonStringInteger), (new JSONObject($jsonStringInteger))->toStdClass());
+		$this->assertEquals(json_decode($jsonStringBool), (new JSONObject($jsonStringBool))->toStdClass());
+		$this->assertEquals(json_decode($jsonStringNull), (new JSONObject($jsonStringNull))->toStdClass());
+		$this->assertEquals(json_decode($jsonStringObject), (new JSONObject($jsonStringObject))->toStdClass());
+		$this->assertEquals(json_decode($jsonStringArray), (new JSONObject($jsonStringArray))->toStdClass());
 		
+		// escaping / quoting
 		$this->assertEquals('\\\n', JSONObject::quote('\n'));
 		$this->assertEquals('\/', JSONObject::quote('/'));
 		$this->assertEquals('\\\\', JSONObject::quote('\\'));
+		
+		// building objects
+		$a = new JSONObject();
+		$a->put('arr', new JSONArray());
+		$a->get('arr')->put('value');
+		
+		$a1 = '{"arr":["value"]}';
+		$this->assertEquals($a1, $a->write());
+		
+		$b = (new JSONObject())->put('obj', new JSONObject('{"a": "1"}'));
+		$b->put("b", 2);
+		
+		$b1 = '{"obj":{"a":"1"},"b":2}';
+		$this->assertEquals($b1, $b->write());
+		
+		// accessing parameters
+		$this->assertEquals("1", $b->get('obj')->get("a"));
+		$this->assertEquals(2, $b->get('b'));
 	}
 }
 
